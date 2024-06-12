b486bf8a422a94f4eebbba85bca4a2aad6e1fcfa
funkatronics <funkatronicsmail@gmail.com>
2024-06-12 17:04:23 -0600

    init

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.publish)
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+val artifactId: String by project
+
+kotlin {
+    jvm {
+        jvmToolchain(11)
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+        macosX64(),
+        macosArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = artifactId
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+mavenPublishing {
+    coordinates(group as String, artifactId, version as String)
+}
