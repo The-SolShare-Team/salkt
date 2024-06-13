d827653cee8280c91195e8237d5e197097328cb8
funkatronics <funkatronicsmail@gmail.com>
2024-06-13 09:11:32 -0600

    add isOnCurve tests

---
+++ src/commonTest/kotlin/com/funkatronics/salt/IsOnCurveTests.kt
@@ -0,0 +1,33 @@
+package com.funkatronics.salt
+
+import com.funkatronics.encoders.Base58
+import com.solana.salt.TweetNaclFast
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class IsOnCurveTests {
+    @Test
+    fun `isOnCurve returns true for known on curve address`() {
+        // given
+        val address = Base58.decode("7aGCrjwMFDLq92oeSwfADozP7RNk5Rkqoz2ESFUZAT3b")
+
+        // when
+        val isOnCurve = TweetNaclFast.isOnCurve(address)
+
+        //
+        assertTrue { isOnCurve }
+    }
+
+    @Test
+    fun `isOnCurve returns false for known off curve address`() {
+        // given
+        val address = Base58.decode("46GZzzetjCURsdFPb7rcnspbEMnCBXe9kpjrsZAkKb6X")
+
+        // when
+        val isOnCurve = TweetNaclFast.isOnCurve(address)
+
+        //
+        assertFalse { isOnCurve }
+    }
+}
